d75a651a34cd64e8ac0dbce6eea9a66120851031
leah-jala <leahlafond@protonmail.com>
2023-04-07 15:42:30 +0000

    Set up product view, urls and directories

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Product, Category
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .forms import AddToBagForm
 
 # Create your views here.
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
