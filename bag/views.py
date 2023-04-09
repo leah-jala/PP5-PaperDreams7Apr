88b923a101d38eaa1070787e504e34382f960b2c
leah-jala <leahlafond@protonmail.com>
2023-04-09 12:46:50 +0000

    Bug - removed js return to scroll position - affected bag total

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 
+
 # Create your views here.
 def view_bag(request):
     """ A view to return the shopping bag page """
 
-    return render (request, 'bag/bag.html')
+    return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
