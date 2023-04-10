6a4a237106ad430d1b79f377c2a31c6faa1d4893
leah-jala <leahlafond@protonmail.com>
2023-04-10 13:54:02 +0000

    Bug fix: move update/remove links so JS will work

--- bag/views.py
+++ bag/views.py
@@ -11,7 +11,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-    
+
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -35,15 +35,19 @@ def adjust_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
-    if quantity > 0:
-        bag[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your bag')
-
-        bag.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your bag')
+    if item_id in bag:
+        if quantity > 0:
+            bag[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
+    else:
+        messages.error(request, f'{product.name} not found in your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
 
 
 def remove_from_bag(request, item_id):
