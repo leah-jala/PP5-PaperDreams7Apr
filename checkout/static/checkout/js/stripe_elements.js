e6016e25e5c5de14cc5bc21340bc034522d210be
leah-jala <leahlafond@protonmail.com>
2023-04-12 20:40:18 +0000

    Added loading overlay to checkout.html

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -51,6 +51,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeOut(100);
+    $('#loading-overlay').fadeIn(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -64,6 +66,8 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeIn(100);
+            $('#loading-overlay').fadeOut(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
