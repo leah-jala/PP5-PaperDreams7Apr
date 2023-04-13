e339fdec8f1ad10b2e29e9f67e0bf14502b9fcf8
leah-jala <leahlafond@protonmail.com>
2023-04-13 07:56:50 +0000

    Register contact model to admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ContactForm
 
-# Register your models here.
+@admin.register(ContactForm)
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ['sent_date', 'last_name', 'first_name', 'email', 'subject', 'newsletter', ]
+    list_filter = ['newsletter', 'sent_date']
+    search_fields = ['first_name', 'last_name', 'email', 'subject']
