e00efe08213948691da273fce9da66bd75fe067c
leah-jala <leahlafond@protonmail.com>
2023-04-15 10:30:39 +0000

    Add learn models and set up admin.py

--- learn/admin.py
+++ learn/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import TutorialCategory, Tutorial, TutorialPost
 
-# Register your models here.
+
+@admin.register(TutorialCategory)
+class TutorialCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
+
+
+@admin.register(Tutorial)
+class TutorialAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'instructor',
+        'category',
+        'difficulty_level',
+        'duration',
+        'created_date')
+    list_filter = ('category', 'difficulty_level')
+    search_fields = ('title', 'instructor__username')
+
+
+@admin.register(TutorialPost)
+class TutorialPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'instructor',
+        'tutorial',
+        'week_number',
+        'posted_date')
+    list_filter = ('tutorial', 'week_number')
+    search_fields = ('title', 'instructor__username')
--- learn/models.py
+++ learn/models.py
@@ -1,3 +1,110 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from djrichtextfield.models import RichTextField
+from django_resized import ResizedImageField
+
+
+class TutorialCategory(models.Model):
+    """
+    A model to enable adding tutorial categories
+    in the admin panel.
+    """
+
+    class Meta:
+        verbose_name_plural = 'Tutorial categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Tutorial(models.Model):
+    """
+    A model to create tutorial content
+    """
+
+    DIFFICULTY_CHOICES = [
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+    ]
+
+    instructor = models.ForeignKey(
+        User,
+        related_name='tutorials',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=100, null=False, blank=False)
+    category = models.ForeignKey(
+        TutorialCategory,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    difficulty_level = models.CharField(
+        max_length=100,
+        choices=DIFFICULTY_CHOICES,
+        default='beginner'
+    )
+    duration = models.CharField(max_length=50, null=False, blank=False)
+    summary = models.CharField(max_length=100, null=False, blank=False)
+    equipment_list = RichTextField(max_length=2000, null=False, blank=False)
+    image = ResizedImageField(
+        size=[400, None],
+        quality=75,
+        upload_to='tutorials/',
+        force_format='WEBP',
+        blank=False,
+        null=False
+    )
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    created_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class TutorialPost(models.Model):
+    """
+    A model to create tutorial posts
+    """
+
+    tutorial = models.ForeignKey(
+        Tutorial,
+        related_name='posts',
+        on_delete=models.CASCADE
+    )
+    instructor = models.ForeignKey(
+        User,
+        related_name='tutorial_posts',
+        on_delete=models.CASCADE
+    )
+    week_number = models.IntegerField()
+    title = models.CharField(max_length=100, null=False, blank=False)
+    instructions = RichTextField(max_length=10000, null=False, blank=False)
+    image = ResizedImageField(
+        size=[400, None],
+        quality=75,
+        upload_to='tutorials/posts/',
+        force_format='WEBP',
+        blank=True,
+        null=True
+    )
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    posted_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['week_number']
+        unique_together = ('tutorial', 'week_number')
+
+    def __str__(self):
+        return f"Week {self.week_number}: {self.title}"
