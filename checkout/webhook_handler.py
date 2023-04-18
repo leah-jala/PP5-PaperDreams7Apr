50eeb2cf5b87dba7e521e37156369a9576538cfb
leah-jala <leahlafond@protonmail.com>
2023-04-18 11:43:54 +0000

    Add profiles app and set up initial mvt and directory

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -71,7 +71,7 @@ class StripeWH_Handler:
                 time.sleep(1)
         if order_exists:
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                content=f'Webhook: {event["type"]} | SUCCESS: Verified in DB',
                 status=200)
         else:
             order = None
@@ -99,7 +99,8 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, quantity in item_data['items_by_size'].items(
+                        ):
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -114,7 +115,7 @@ class StripeWH_Handler:
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=f'Webhook: {event["type"]} | SUCCESS: Created order in WH',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
--- paperdreams/settings.py
+++ paperdreams/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'pages',
     'blog',
     'tutorials',
+    'profiles',
 
     # Other
     'crispy_forms',
