2202fc86014236c0ff73507aa6855be436ffccce
leah-jala <leahlafond@protonmail.com>
2023-04-22 09:45:40 +0000

    Add aria-labels to homepage images, remove reviews link

--- paperdreams/settings.py
+++ paperdreams/settings.py
@@ -19,14 +19,13 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-HEROKU_API_KEY = os.environ.get('HEROKU_API_KEY')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+HEROKU_API_KEY = os.environ.get('HEROKU_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
