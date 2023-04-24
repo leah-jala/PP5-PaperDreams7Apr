b0093fd78b446ee800bd8a9f06561302ffafbd38
leah-jala <leahlafond@protonmail.com>
2023-04-24 16:40:05 +0000

    Add wishlist views

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from products.models import Product
+from .models import Wishlist
 
-# Create your views here.
+
+@login_required
+def view_wishlist(request):
+    """ A view to return the wishlist page """
+    wishlist, _ = Wishlist.objects.get_or_create(user=request.user)
+    context = {
+        'wishlist': wishlist,
+    }
+    return render(request, 'wishlist/view_wishlist.html', context)
+
+
+@login_required
+def add_to_wishlist(request, item_id):
+    """ Add a product to the wishlist """
+    product = get_object_or_404(Product, pk=item_id)
+    wishlist, _ = Wishlist.objects.get_or_create(user=request.user)
+    wishlist.products.add(product)
+    return redirect(reverse('view_wishlist'))
+
+
+@login_required
+def remove_from_wishlist(request, item_id):
+    """ Remove a product from the wishlist """
+    product = get_object_or_404(Product, pk=item_id)
+    wishlist, _ = Wishlist.objects.get_or_create(user=request.user)
+    wishlist.products.remove(product)
+    return redirect(reverse('view_wishlist'))
