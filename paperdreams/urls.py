bc56d645cf4e226b5d5cbe18de4ba5f71954b00a
leah-jala <leahlafond@protonmail.com>
2023-04-24 16:41:15 +0000

    Add wishlist urls paths

--- paperdreams/urls.py
+++ paperdreams/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('djrichtextfield/', include('djrichtextfield.urls')),
+    path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
