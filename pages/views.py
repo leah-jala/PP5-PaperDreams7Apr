cc225cb74de1d389d770a5a5fc9345cf5aa140da
leah-jala <leahlafond@protonmail.com>
2023-04-26 08:13:09 +0000

    Pep8 fixes on home and pages app files

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+
 def about(request):
     return render(request, 'pages/about.html')
 
+
 def shipping_returns(request):
     return render(request, 'pages/shipping_returns.html')
+
 
 def privacy(request):
     return render(request, 'pages/privacy.html')
