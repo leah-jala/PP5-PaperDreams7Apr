b5ae893d666306b6628e6e8e045bf9eb9ff6ccef
leah-jala <leahlafond@protonmail.com>
2023-04-27 08:27:36 +0000

    Add missing docstrings to home  app file

--- home/apps.py
+++ home/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """
+    AppConfig for the home app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
