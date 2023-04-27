3aaf16c2b8a9279bb48e1c6b62c3be11ca382c2d
leah-jala <leahlafond@protonmail.com>
2023-04-27 06:47:15 +0000

    Add doc strings to pages/views

--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 
 
 def about(request):
+    """ Render About page """
     return render(request, 'pages/about.html')
 
 
 def shipping_returns(request):
+    """ Render Delivery and Returns page """
     return render(request, 'pages/shipping_returns.html')
 
 
 def privacy(request):
+    """ Render Privacy page """
     return render(request, 'pages/privacy.html')
