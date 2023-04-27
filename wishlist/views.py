ed78c0265300126580d166e9aba7f33ea152aa14
leah-jala <leahlafond@protonmail.com>
2023-04-27 09:28:44 +0000

    Change wishlist view for case item is already in shopping bag

--- wishlist/views.py
+++ wishlist/views.py
@@ -20,9 +20,23 @@ def add_to_wishlist(request, item_id):
     """ Add a product to the wishlist """
     product = get_object_or_404(Product, pk=item_id)
     wishlist, _ = Wishlist.objects.get_or_create(user=request.user)
-    wishlist.products.add(product)
-    messages.success(request, 'Successfully added to wishlist!')
-    return redirect(reverse('view_wishlist'))
+
+    # Check if the item is already in the bag
+    bag = request.session.get('bag', {})
+    if str(item_id) in bag:
+        messages.info(
+            request, (
+                'This item is already in your bag! '
+                'Remove it from your bag, then add it '
+                'to your wishlist if you are not '
+                'ready to buy now.'
+            )
+        )
+    else:
+        wishlist.products.add(product)
+        messages.success(request, 'Added to wishlist!')
+    # Redirect the user back to the current page
+    return redirect(request.META.get('HTTP_REFERER', 'home'))
 
 
 @login_required
