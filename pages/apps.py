7599125af3c12eb1acf65a85b24c71cca8e6b7bb
leah-jala <leahlafond@protonmail.com>
2023-04-27 08:28:30 +0000

    Add missing docstrings to pages app file

--- pages/apps.py
+++ pages/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class PagesConfig(AppConfig):
+    """
+    AppConfig for the pages app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
