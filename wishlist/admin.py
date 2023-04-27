548ee8857b52a5bc0635a2184818dfc8bf9fdc0f
leah-jala <leahlafond@protonmail.com>
2023-04-27 08:44:52 +0000

    Add missing docstrings to wishlist files

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -11,11 +11,17 @@ class WishlistAdmin(admin.ModelAdmin):
     readonly_fields = ('date_added',)
 
     def product_list(self, obj):
+        """
+        Return a comma-separated list of the products in the wishlist.
+        """
         return "\n".join([p.name for p in obj.products.all()])
 
     product_list.short_description = "Products"
 
     def formatted_date_added(self, obj):
+        """
+        Return the date added to the wishlist in a human-readable format.
+        """
         return obj.date_added.strftime("%b %d, %Y %H:%M:%S")
 
     formatted_date_added.short_description = "Date added"
--- wishlist/apps.py
+++ wishlist/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class WishlistConfig(AppConfig):
+    """
+    AppConfig for the tutorials app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wishlist'
